9281cfdc7847843513c04362e2415fcd1f99b7da
DIEGO UNDURRAGA <dundurraga@ESM.UCSB.EDU>
2018-08-13 14:20:47 -0700

    prueba del tercer commit

--- First_script.R
+++ First_script.R
@@ -1,6 +1,16 @@
 ## Test
 ## Diego
 
-a = 1
+a = 8
 b = 2 
 
+c = 5
+
+answer = a*b
+
+fahrenheit_to_kelvin <- function(temp_F) {
+  temp_K <- ((temp_F - 32) * (5 / 9)) + 273.15
+  return(temp_K)
+}
+
+fahrenheit_to_kelvin(34)
